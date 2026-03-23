9c4357810fbf09f3b04b155a0a730ff2a6b74481
Ty Wedel <tywedel0@gmail.com>
2026-03-23 12:34:18 -0700

    fix admin-login page dynamic

--- app/admin-login/page.tsx
+++ app/admin-login/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { redirect } from "next/navigation";
 import { getCurrentUser } from "@/lib/auth";
 import AdminLoginForm from "@/components/AdminLoginForm";
